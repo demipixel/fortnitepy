c626e456483ee660ca1d81efcbbc9245e3fc2e1f
Terbau <bragebau@hotmail.no>
2020-08-20 15:26:19 +0200

    Update viewcode.py

--- docs/localexts/viewcode.py
+++ docs/localexts/viewcode.py
@@ -201,9 +201,9 @@ def doctree_read(app: Sphinx, doctree: Node) -> None:
             print('REFID:', fullname)
             print('REFDOC:', env.docname)
             inline = nodes.inline('', _('[github]'), classes=['viewcode-link'])
-            onlynode += addnodes.pending_xref('', inline, reftype='viewcode', refdomain='std',
-                                              refexplicit=False, reftarget=github_link,
-                                              refid=fullname, refdoc=env.docname)
+            onlynode += addnodes.manpage('', inline, reftype='viewcode', refdomain='std',
+                                         refexplicit=False, reftarget=github_link,
+                                         refid=fullname, refdoc=env.docname)
             signode += onlynode
 
     # print('ENV VIEWCODE MODULES', json.dumps(env._viewcode_modules, indent=2, default=lambda o: str(o)))
