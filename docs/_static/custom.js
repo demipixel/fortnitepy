9b0aab83ccfb6ec15740a7f0c168709a28380b43
Terbau <bragebau@hotmail.no>
2020-08-23 19:51:58 +0200

    Update custom.js

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,31 +1,36 @@
 $(document).ready(function () {
-    var sections = $('div.section');
-    var activeLink = null;
-    var bottomHeightThreshold = $(document).height() - 30;
-  
-    $(window).scroll(function (event) {
-      var distanceFromTop = $(this).scrollTop();
-      var currentSection = null;
-  
-      if(distanceFromTop + window.innerHeight > bottomHeightThreshold) {
-        currentSection = $(sections[sections.length - 1]);
-      }
-      else {
-        sections.each(function () {
-          var section = $(this);
-          if (section.offset().top - 1 < distanceFromTop) {
-            currentSection = section;
-          }
-        });
-      }
-  
-      if (activeLink) {
-        activeLink.parent().removeClass('active');
-      }
-  
-      if (currentSection) {
-        activeLink = $('.sphinxsidebar a[href="#' + currentSection.attr('id') + '"]');
-        activeLink.parent().addClass('active');
-      }
-    });
+  var sections = $('div.section');
+  var activeLink = null;
+  var bottomHeightThreshold = $(document).height() - 30;
+
+  $(window).scroll(function (event) {
+    var distanceFromTop = $(this).scrollTop();
+    var currentSection = null;
+
+    if(distanceFromTop + window.innerHeight > bottomHeightThreshold) {
+      currentSection = $(sections[sections.length - 1]);
+    }
+    else {
+      sections.each(function () {
+        var section = $(this);
+        if (section.offset().top - 1 < distanceFromTop) {
+          currentSection = section;
+        }
+      });
+    }
+
+    if (activeLink) {
+      activeLink.parent().removeClass('active');
+    }
+
+    if (currentSection) {
+      activeLink = $('.sphinxsidebar a[href="#' + currentSection.attr('id') + '"]');
+      activeLink.parent().addClass('active');
+    }
   });
+
+  $('.source-link').parent().click(function(event) {
+    event.preventDefault();
+    window.location.href = $(this).parent().attr('href') + '?referrer=test';
+  });
+});
